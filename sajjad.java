8ab785fd6368dfce86256cd41828e6fc29c5d4ee
Sajjad Bhuiyan <95546486+SBwho@users.noreply.github.com>
2023-04-26 21:21:49 +0200

    Update sajjad.java

--- sajjad.java
+++ sajjad.java
@@ -1,8 +1,7 @@
-class sajjad{
+class sajjad {
     public static void main(String[] args) {
-        System.out.println("this is sajjad Branch");
-        transaction sajjadTransaction= new transaction("sajjad", "pinky", 10);
-        String s= sajjadTransaction.calculateHash();
-        System.out.println("hash code is : "+ s);
+        transaction sajjadTransaction= new transaction("sajjad", "piunky", 10);
+       String s= sajjadTransaction.calculateHash();
+       System.out.println("hash code is: " + s);
     }
 }
