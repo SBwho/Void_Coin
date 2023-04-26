d3eb934be479896762ae1577ac143daba768ec02
Sajjad Bhuiyan <95546486+SBwho@users.noreply.github.com>
2023-04-26 21:14:27 +0200

    Update sajjad.java

--- sajjad.java
+++ sajjad.java
@@ -1,4 +1,4 @@
-class Test {
+public class Test {
     public static void main(String[] args) {
         System.out.println("this is sajjad Branch");
         transaction sajjadTransaction= new transaction("sajjad", "pinky", 10);
