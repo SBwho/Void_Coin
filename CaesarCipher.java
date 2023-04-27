d4ddc6214cb4062bfe1ebd4ade72fa96178557ef
Abdullah9431 <abdaraz10@gmail.com>
2023-04-27 01:37:14 +0000

    ok

--- CaesarCipher.java
+++ CaesarCipher.java
@@ -1,24 +1,21 @@
 import java.security.MessageDigest;
 import java.util.Arrays;
-import java.lang.Integer;
 
 public class CaesarCipher {
     public static String hash_repr_hex(byte[] hashed){
         String sum = "";
-        for (int x : hashed) {
-            //System.out.println(Integer.toHexString(x));
-            x=(x<0)?-x:x;
-            sum = sum + Integer.toHexString(x);
+        for (byte x : hashed) {
+            sum = sum + String.format("%02x", x);
         }
         return sum;
     }
     public static void main(String[] args) {
-        String initial_message = "This is a secret message";
+        String initial_message = "a";
         try{
         byte[] message = initial_message.getBytes("UTF-8");
 
         // As MessageDigest is an abstract class it cannot be instantiated 
-        MessageDigest hasher = MessageDigest.getInstance("SHA-512");
+        MessageDigest hasher = MessageDigest.getInstance("SHA-256");
         System.out.println(hasher.getDigestLength());
         byte[] hashed_data = hasher.digest(message);
         System.out.println(Arrays.toString(hashed_data));
